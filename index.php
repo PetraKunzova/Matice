6aa90d4526859feb04687f14367ab030e0e79118
PetraKunzova <95088082+PetraKunzova@users.noreply.github.com>
2022-01-18 20:51:49 +0100

    Add files via upload
    Vícerozměrné pole, jehož velikost určuje uživatel zadáním rozměrů do formuláře. Pole je pak naplněno náhodnými čísly.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="cs-cz">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Min Max</title>
+        <link href="styl.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Vyhledání minima a maxima v matici</h1>
+        <?php
+        
+        
+       
+      
+        ?>
+        <form method="POST" action="matice.php">
+            Počet sloupců<br />
+            <input name="sloupec" type="number" /><br />
+            Počet řádků<br />
+            <input name="radek" type="number" /><br /><br />
+            <input type="submit" value="Vytvoř pole" />
+        </form>
+    </body>
+</html>
