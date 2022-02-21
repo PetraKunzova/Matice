09a80a7cc1922fc66b2f7f9f364abedba024aaf6
PetraKunzova <95088082+PetraKunzova@users.noreply.github.com>
2022-02-21 12:11:37 +0100

    Add files via upload

--- matice.php
+++ matice.php
@@ -13,27 +13,47 @@
         $matice = [[]];         //@param pole
         $maxElement = PHP_INT_MIN;
         $minElement = PHP_INT_MAX;
-        //Vytvoříme tabulku
-        echo('<table border="1">'); 
+        $imin = $imax = -1;
+        $jmin = $jmax = -1;
+
+        //cyklus pro vytvoření pole 
         for ($i = 0; $i < $r; $i++) {
-            echo('<tr>');
             for ($j = 0; $j < $s; $j++) {
-             if  ($matice[$i][$j] = rand(-100, 100));
-                echo('<td>' . $matice[$i][$j] . '</td>');
+                $matice[$i][$j] = rand(-100, 100);
                 if ($matice[$i][$j] < $minElement) {
                     /** @var type $minElement */
                     $minElement = $matice[$i][$j];
+                    $imin = $i;
+                    $jmin = $j;
                 }
                 if ($matice[$i][$j] > $maxElement) {
                     /** @var type $maxElement */
                     $maxElement = $matice[$i][$j];
+                    $imax = $i;
+                    $jmax = $j;
                 }
+            }
+        }
+
+        echo('<table border="2">');
+        for ($i = 0; $i < $r; $i++) {
+            echo('<tr>');
+            for ($j = 0; $j < $s; $j++) {
+                if (($i == $imin) && ($j == $jmin)) {
+                    if (($imin == $imax) && ($jmin == $jmax)) {
+                        echo('<td style="color:#FFA500">' . $matice[$i][$j] . '</td>');
+                    } else
+                        echo('<td style="color:#ff0000">' . $matice[$i][$j] . '</td>');
+                } elseif (($i == $imax) && ($j == $jmax)) {
+                    echo('<td style="color:#006400">' . $matice[$i][$j] . '</td>');
+                } else
+                    echo('<td>' . $matice[$i][$j] . '</td>');
             }
             echo('</tr>');
         }
         echo('</table>');
 
-        echo("Maximus v poli je " . $maxElement . " a minimus je " . $minElement . " .<br />");
+        echo("<p>Maximus v poli je " . $maxElement . " a minimus je " . $minElement . " .<br /></p>");
         echo('<br />');
         ?>
 
